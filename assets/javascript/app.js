05022ff9fb6446830a003cce864d5198adb4a7f0
Joseph Kim <gtburneraddy@gmail.com>
2018-03-24 11:31:29 -0400

    wip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,30 @@
   };
   firebase.initializeApp(config);
 
-  $("").on("click" () {
-  	
-  });
+  var database = firebase.database();
+
+  $("#add-employee-btn").on("click", function(event) {
+  event.preventDefault();
+
+    // Grabs user input
+  var empName = $("#employee-name-input").val().trim();
+  var empRole = $("#role-input").val().trim();
+  var empStart = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
+  var empRate = $("#rate-input").val().trim();
+
+    // Creates local "temporary" object for holding employee data
+  var newEmp = {
+    name: empName,
+    role: empRole,
+    start: empStart,
+    rate: empRate
+  };
+
+    // Uploads employee data to the database
+  database.ref().push(newEmp);
+
+  // Logs everything to console
+  console.log(newEmp.name);
+  console.log(newEmp.role);
+  console.log(newEmp.start);
+  console.log(newEmp.rate);
