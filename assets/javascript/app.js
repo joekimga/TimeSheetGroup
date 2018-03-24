6254f4e1701a19a3895812164c7d6650d2273a29
Joseph Kim <gtburneraddy@gmail.com>
2018-03-24 11:23:56 -0400

    wip

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+ 
+//initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAmzFi9GKoB7V6APAZr3goNsZnRGlaDCXA",
+    authDomain: "maidenvoyage-9fa43.firebaseapp.com",
+    databaseURL: "https://maidenvoyage-9fa43.firebaseio.com",
+    projectId: "maidenvoyage-9fa43",
+    storageBucket: "maidenvoyage-9fa43.appspot.com",
+    messagingSenderId: "795370132163"
+  };
+  firebase.initializeApp(config);
+
+  $("").on("click" () {
+  	
+  });
